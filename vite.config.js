556696600147ac57b090c0f2bc590d19adb1ac5c
zhouzhenhua <zhouzhenhua1984@163.com>
2026-03-17 16:24:24 +0800

    初次提交

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [vue()],
+    server: {
+        host: '0.0.0.0',   // 允许通过局域网 IP 访问
+        port: 5173         // 可选：指定端口
+    },
+})
